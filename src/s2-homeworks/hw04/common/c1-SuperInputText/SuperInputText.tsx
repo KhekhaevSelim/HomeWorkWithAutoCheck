f7b17173844fa69aa60e619d4d4455b824c5f349
KhekhaevSelim <selim16695@mail.ru>
2023-01-26 20:28:42 +0300

    HW04secondRound

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -50,9 +50,10 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
 
     const finalSpanClassName = s.error
         + (spanClassName ? ' ' + spanClassName : '')
-    const finalInputClassName = s.input
-        + (error ? ' ' + s.errorInput : ' ' + s.superInput)
-        + (className ? ' ' + s.className : '') // задача на смешивание классов
+    const finalInputClassName = `${s.input} ${error? s.errorInput : s.superInput}`
+    // const finalInputClassName = s.input
+    //     + (error ? ' ' + s.errorInput : ' ' + s.superInput)
+    //     + (className ? ' ' + s.className : '') // задача на смешивание классов
 
     return (
         <div>
