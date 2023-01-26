10e9876e656cd3fa1c131b26454bf987238c7518
KhekhaevSelim <selim16695@mail.ru>
2023-01-26 20:40:48 +0300

    HW04thirdRound

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -93,7 +93,7 @@ const Stand = () => {
                     <SuperCheckbox
                         id={'hw4-super-checkbox-like-old'}
                         checked={stateForAllCheckboxes}
-                        onChange={(e) => setChecked(e.currentTarget.checked)}
+                        // onChange={(e) => setChecked(e.currentTarget.checked)}
                     />
                 </div>
             </div>
