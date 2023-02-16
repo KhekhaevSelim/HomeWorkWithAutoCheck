be831441f876ecefb3b14ed7c697b561ea6a833e
KhekhaevSelim <selim16695@mail.ru>
2023-02-16 14:04:29 +0300

    HW7-03

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -59,7 +59,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       {...restProps}
                   />
                   <span
-                      id={id + '-span-' + o.id}
+                      id={id + '-span-' + o.value}
                       {...spanProps}
                       className={spanClassName}
                   >
