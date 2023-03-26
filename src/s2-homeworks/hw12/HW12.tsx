45f283932296a3a12e62b40026fe6873a4ebeaca
KhekhaevSelim <selim16695@mail.ru>
2023-03-27 00:37:59 +0300

    HW-12-3

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -24,8 +24,8 @@ const HW12 = () => {
     // взять ид темы из редакса
     const themeId = useSelector<AppStoreType, number>(state => state.theme.themeId)
     const dispatch = useDispatch()
-    const change = (id: number) => { // дописать функцию
-    dispatch(changeThemeId(id))
+    const change = (id: string) => { // дописать функцию
+    dispatch(changeThemeId(+id))
     }
 
     useEffect(() => {
